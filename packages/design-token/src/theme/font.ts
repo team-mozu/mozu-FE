2b68dd8e3a49e4dff0ff72eacdede5af184e10f1
seungWoo <ssounsowns@gmail.com>
2025-01-06 00:57:42 +0900

    feat: design-token color, font 추가
    design-token 패키지에 color, font 추가
    
    - color: orange, zinc, blue, green, yellow 색상 추가
    - font: h1 ~ l2 폰트 추가
    - 추후 컴포넌트에서 사용 예정

--- packages/design-token/src/theme/font.ts
+++ packages/design-token/src/theme/font.ts
@@ -1,1 +1,22 @@
-export const font = {};
+const fontToCss = (weight: number, size: number, lineHeight: number) => {
+  return {
+    fontWeight: `${weight}px`,
+    fontSize: `${size}px`,
+    lineHeight: `${lineHeight}px`,
+  };
+};
+
+export const font = {
+  h1: fontToCss(600, 40, 48),
+  h2: fontToCss(600, 36, 42),
+  h3: fontToCss(600, 28, 36),
+  h4: fontToCss(500, 24, 32),
+  t1: fontToCss(600, 20, 24),
+  t2: fontToCss(500, 20, 24),
+  t3: fontToCss(500, 18, 20),
+  t4: fontToCss(400, 18, 20),
+  b1: fontToCss(500, 16, 20),
+  b2: fontToCss(400, 18, 20),
+  l1: fontToCss(500, 14, 18),
+  l2: fontToCss(400, 14, 18),
+};
