7263732edf2b79158cc6bc22e792d94edceb3085
do-hyun123 <shickcoong777@gmail.com>
2025-01-05 23:27:03 +0900

    feat : husky, commit lint 추가

---
+++ commitlint.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: [],
+  rules: {
+    'type-enum': [2, 'always'],
+    'colon-not-include': [2, 'always'],
+  },
+  plugins: [
+    {
+      rules: {
+        'colon-not-include': ({ header }) => {
+          return [header.includes(' : '), `:이 포함되지 않았습니다.`];
+        },
+        'type-enum': ({ header }) => {
+          const types = new Set([
+            'build',
+            'ci',
+            'chore',
+            'docs',
+            'feat',
+            'fix',
+            'perf',
+            'refactor',
+            'style',
+            'test',
+          ]);
+          return [
+            types.has(header.split(' : ')[0]),
+            `${header.split(' : ')[0]}은 type으로 사용할 수 없습니다.`,
+          ];
+        },
+      },
+    },
+  ],
+};
