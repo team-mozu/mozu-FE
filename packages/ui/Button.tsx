4e75dd75ebbd2b99485f05aa363fe925afd85738
박지연 <163213733+j02on@users.noreply.github.com>
2025-01-06 20:11:04 +0900

    feat : Icon 유무 처리
    Button의 icon 선택 처리를 하였습니다.

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,12 +1,17 @@
 import { React } from 'react';
 import styled from '@emotion/styled';
-import { color } from '@mozu/design-token';
+import { color, font } from '@mozu/design-token';
+import { Del, Edit, Plus, Save, Start } from './assets';
 
 type ButtonType = {
   children?: string;
   backgroundColor?: string;
   color?: string;
   borderColor?: string;
+  iconSize?: number;
+  iconColor?: string;
+  type?: 'startImg' | 'delImg' | 'editImg' | 'plusImg' | 'saveImg';
+  isIcon?: boolean;
 };
 
 export const Button = ({
@@ -14,7 +19,19 @@ export const Button = ({
   backgroundColor,
   color,
   borderColor,
+  iconSize,
+  iconColor,
+  type = 'startImg',
+  isIcon = false,
 }: ButtonType) => {
+  const buttonIconType = {
+    startImg: <Start size={iconSize} color={iconColor} />,
+    delImg: <Del size={iconSize} color={iconColor} />,
+    editImg: <Edit size={iconSize} color={iconColor} />,
+    plusImg: <Plus size={iconSize} color={iconColor} />,
+    saveImg: <Save size={iconSize} color={iconColor} />,
+  };
+
   return (
     <ButtonContainer
       backgroundColor={backgroundColor}
@@ -22,6 +39,7 @@ export const Button = ({
       borderColor={borderColor}
     >
       {children}
+      {isIcon && buttonIconType[type]}
     </ButtonContainer>
   );
 };
@@ -35,4 +53,6 @@ const ButtonContainer = styled.button<Omit<ButtonType, 'children'>>`
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 6px;
+  font: ${font.b1};
 `;
