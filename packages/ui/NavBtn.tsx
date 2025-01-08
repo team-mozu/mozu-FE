a69f449899fc7beaa5088156a0ddd1c27ab03d3c
박지연 <163213733+j02on@users.noreply.github.com>
2025-01-08 17:54:28 +0900

    feat : SideBar 퍼블리싱

---
+++ packages/ui/NavBtn.tsx
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+import { color } from '@mozu/design-token';
+import { ReactNode } from 'react';
+import { ArticleIcon, ClassIcon, StockIcon } from './assets';
+
+type NavType = {
+  children?: ReactNode;
+  isColor?: boolean;
+  type?: 'classIcon' | 'stockIcon' | 'articleIcon';
+};
+
+export const NavBtn = ({ children, isColor, type }: NavType) => {
+  const getIconColor = () => (isColor ? color.orange[600] : color.zinc[500]);
+
+  const buttonIconType = {
+    classIcon: <ClassIcon color={getIconColor()} />,
+    stockIcon: <StockIcon color={getIconColor()} />,
+    articleIcon: <ArticleIcon color={getIconColor()} />,
+  };
+
+  return (
+    <NavContent isColor={isColor}>
+      {buttonIconType[type]}
+      {children}
+    </NavContent>
+  );
+};
+
+const NavContent = styled.button<Pick<NavType, 'isColor'>>`
+  width: 100%;
+  height: 52px;
+  background-color: ${({ isColor }) =>
+    isColor ? color.orange[50] : 'transparent'};
+  color: ${({ isColor }) => (isColor ? color.orange[600] : color.zinc[500])};
+  display: flex;
+  gap: 12px;
+  padding-left: 20px;
+  align-items: center;
+  justify-content: start;
+`;
--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,3 +1,5 @@
 export * from './Button';
 export * from './ClassPost';
 export * from './Header';
+export * from './NavBtn';
+export * from './SideBar';
