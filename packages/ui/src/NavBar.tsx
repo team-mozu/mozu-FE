a570576fa63bca18aff9c24e3641cc704335f2af
박지연 <163213733+j02on@users.noreply.github.com>
2025-01-28 18:09:20 +0900

    feat : navbar 형식 수정

--- packages/ui/src/NavBar.tsx
+++ packages/ui/src/NavBar.tsx
@@ -1,48 +1,36 @@
 import styled from '@emotion/styled';
 import { color, font } from '@mozu/design-token';
-import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 interface INavType {
   children: string;
-  isPath: boolean;
+  isActive: boolean;
+  onClick: () => void;
 }
 
-const Nav = ({ isPath, children }: INavType) => {
-  return <NavContainer isPath={isPath}>{children}</NavContainer>;
+const Nav = ({ isActive, children, onClick }: INavType) => {
+  return (
+    <NavContainer isActive={isActive} onClick={onClick}>
+      {children}
+    </NavContainer>
+  );
 };
 
 export const NavBar = () => {
-  const [isCurrentPricePath, setIsCurrentPricePath] = useState<boolean>(false);
-  const [isItemPath, setIsItemPath] = useState<boolean>(false);
-  const [isNewsPath, setIsNewsPath] = useState<boolean>(false);
+  const [activeIndex, setActiveIndex] = useState<number>(0);
 
-  const { pathname } = useLocation();
+  const navName = ['시세정보', '종목정보', '뉴스'];
 
-  useEffect(() => {
-    if (pathname.includes('/current-price')) {
-      //시세정보 path
-      setIsCurrentPricePath(true);
-      setIsItemPath(false);
-      setIsNewsPath(false);
-    } else if (pathname.includes('/item')) {
-      // 종목정보 path
-      setIsCurrentPricePath(false);
-      setIsItemPath(true);
-      setIsNewsPath(false);
-    } else if (pathname.includes('/news')) {
-      // 뉴스 path
-      setIsCurrentPricePath(false);
-      setIsItemPath(false);
-      setIsNewsPath(true);
-    }
-  }, [pathname]);
-
+  const navClick = (index: number) => {
+    setActiveIndex(index);
+  };
   return (
     <BarContainer>
-      <Nav isPath={isCurrentPricePath}>시세정보</Nav>
-      <Nav isPath={isItemPath}>종목정보</Nav>
-      <Nav isPath={isNewsPath}>뉴스</Nav>
+      {navName.map((data, index) => (
+        <Nav onClick={() => navClick(index)} isActive={index === activeIndex}>
+          {data}
+        </Nav>
+      ))}
     </BarContainer>
   );
 };
@@ -53,7 +41,7 @@ const BarContainer = styled.div`
   justify-content: center;
 `;
 
-const NavContainer = styled.button<Pick<INavType, 'isPath'>>`
+const NavContainer = styled.button<Pick<INavType, 'isActive'>>`
   outline: none;
   border: none;
   padding: 10px 16px;
@@ -61,8 +49,8 @@ const NavContainer = styled.button<Pick<INavType, 'isPath'>>`
   justify-content: center;
   align-items: center;
   border-radius: 36px;
-  color: ${({ isPath }) => (isPath ? color.black : color.zinc[600])};
+  color: ${({ isActive }) => (isActive ? color.black : color.zinc[600])};
   font: ${font.b1};
-  background-color: ${({ isPath }) =>
-    isPath ? color.zinc[200] : 'transparent'};
+  background-color: ${({ isActive }) =>
+    isActive ? color.zinc[200] : 'transparent'};
 `;
