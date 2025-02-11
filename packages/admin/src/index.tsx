a26ecb3b45fcba952fc3845d43cf8d2307b03a0a
do-hyun123 <shickcoong777@gmail.com>
2025-02-11 21:38:57 +0900

    hotfix : admin 코드 수정

--- packages/admin/src/index.tsx
+++ packages/admin/src/index.tsx
@@ -1,6 +1,7 @@
 import App from './App';
 import CreateDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GlobalStyle } from '@mozu/design-token';
 
 const root = CreateDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -18,6 +19,7 @@ const queryClient = new QueryClient({
 
 root.render(
   <QueryClientProvider client={queryClient}>
+    <GlobalStyle />
     <App />
   </QueryClientProvider>,
 );
