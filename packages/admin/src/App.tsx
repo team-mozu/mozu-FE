456b40c1cab1a81d8b09d6f3e7b27d100808a726
dutexion <dutexion@gmail.com>
2025-04-23 09:10:06 +0900

    refactor :: 구조 개선

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -1,8 +1,19 @@
-import { RouterProvider } from 'react-router-dom';
-import { Router } from './router';
+import { RouterProvider } from "react-router-dom";
+import { Router } from "./router";
+import { queryClient } from "./utils/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { StyledProvider } from "./style/StyledProvider";
 
 function App() {
-  return <RouterProvider router={Router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <StyledProvider>
+        <RouterProvider router={Router} />
+      </StyledProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
