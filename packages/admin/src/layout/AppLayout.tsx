3eda2738daa4ced7dd4bbdbaf5a06dcece862285
dutexion <dutexion@gmail.com>
2025-04-23 10:24:28 +0900

    chore :: (어드민) 쿠키가 없거나 Root 경로에 있다면 root으로 이동

--- packages/admin/src/layout/AppLayout.tsx
+++ packages/admin/src/layout/AppLayout.tsx
@@ -1,15 +1,26 @@
-import { SideBar, Header } from '@mozu/ui';
-import { Outlet } from 'react-router-dom';
-import styled from '@emotion/styled';
+import { SideBar, Header } from "@mozu/ui";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import styled from "@emotion/styled";
+import { useEffect } from "react";
+import { getCookies } from "@configs/util";
 
 export const AppLayout = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const accessToken = getCookies<string>("accessToken");
+
+  useEffect(() => {
+    if (accessToken === undefined) navigate("signin", { replace: true });
+    if (pathname === "/") navigate("/class-management", { replace: true });
+  }, [pathname, navigate, accessToken]);
+
   return (
     <AppContainer>
       <Header isAdmin={true} />
       <SideBar
-        name={'대전시 진로융합원 창업마을'}
-        role={'관리자'}
-        navTitle={'관리'}
+        name={"대전시 진로융합원 창업마을"}
+        role={"관리자"}
+        navTitle={"관리"}
       />
       <MainContent>
         <Outlet />
