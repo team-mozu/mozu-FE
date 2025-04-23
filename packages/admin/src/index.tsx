597c37371188bd0a19b32247c3a89f479243cb66
dutexion <dutexion@gmail.com>
2025-04-23 09:13:08 +0900

    refactor :: delete unUsed dependency in Index.tsx

--- packages/admin/src/index.tsx
+++ packages/admin/src/index.tsx
@@ -1,9 +1,5 @@
 import App from "./App";
 import CreateDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { GlobalStyle } from "@mozu/design-token";
-import { CustomToastContainer } from "@mozu/ui";
 
 CreateDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
