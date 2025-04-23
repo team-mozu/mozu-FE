a80d637e31b59443ad2afde45de690211bed90fa
dutexion <dutexion@gmail.com>
2025-04-23 13:58:55 +0900

    chore :: stockinfo err fix

--- packages/student/src/components/home/stock/StockInfo.tsx
+++ packages/student/src/components/home/stock/StockInfo.tsx
@@ -1,9 +1,9 @@
-import { font } from '@mozu/design-token';
-import { Accounts } from '@mozu/ui';
-import styled from '@emotion/styled';
-import { color } from '@mozu/design-token';
-import { useGetStockDetail } from '@/apis';
-import { useParams } from 'react-router-dom';
+import { font } from "@mozu/design-token";
+import { Accounts } from "@mozu/ui";
+import styled from "@emotion/styled";
+import { color } from "@mozu/design-token";
+import { useGetStockDetail } from "@/apis";
+import { useParams } from "react-router-dom";
 
 export const StockInfo = () => {
   const { stockId } = useParams();
@@ -15,11 +15,7 @@ export const StockInfo = () => {
       <CompanyInfo>
         <Label>회사 정보</Label>
         <div>
-          <StyledText>
-            {data?.itemInfo
-              ? JSON.parse(`"${data.itemInfo.replace(/"/g, '\\"')}"`)
-              : ''}
-          </StyledText>
+          <StyledText>{data?.itemInfo}</StyledText>
         </div>
       </CompanyInfo>
       <CompanyMain>
@@ -27,18 +23,18 @@ export const StockInfo = () => {
           <div>
             <Label>재무상태표</Label>
             <ContentWrapper>
-              <Accounts title={'부채'} content={data?.debt ?? 0} />
-              <Accounts title={'자본금'} content={data?.capital ?? 0} />
+              <Accounts title={"부채"} content={data?.debt ?? 0} />
+              <Accounts title={"자본금"} content={data?.capital ?? 0} />
             </ContentWrapper>
           </div>
 
           <div>
             <Label>손익계산서</Label>
             <ContentWrapper>
-              <Accounts title={'매출액'} content={data?.profit ?? 0} />
-              <Accounts title={'매출원가'} content={data?.profitOG ?? 0} />
-              <Accounts title={'매출이익'} content={data?.profitBen ?? 0} />
-              <Accounts title={'당기순이익'} content={data?.netProfit ?? 0} />
+              <Accounts title={"매출액"} content={data?.profit ?? 0} />
+              <Accounts title={"매출원가"} content={data?.profitOG ?? 0} />
+              <Accounts title={"매출이익"} content={data?.profitBen ?? 0} />
+              <Accounts title={"당기순이익"} content={data?.netProfit ?? 0} />
             </ContentWrapper>
           </div>
         </Section>
