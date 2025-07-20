341e7b022e892176aa4d59b5350ca9231514e61d
jidohyun <shickcoong777@gmail.com>
2025-07-20 19:10:34 +0900

    refactor(student): useHistory 훅 제거 및 관련 파일 정리

--- packages/student/src/hook/index.ts
+++ packages/student/src/hook/index.ts
@@ -1,7 +1,5 @@
 export * from "./useAsyncButton";
 export * from "./useForm";
-export * from "./useHistory";
-export * from "./useLocalStorage";
 export * from "./useLocalStorage";
 export * from "./useSSE";
 export * from "./useUnchangedValue";
