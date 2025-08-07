6161cd057fb03ce876701a8697ebb7b7be94a8c4
seungW0o <ssounsowns@gmail.com>
2025-08-07 22:57:24 +0900

    chore(config): react-cosmos 설정 및 실행 스크립트 추가마무리
    ui 패키지에서만 react-cosmos를 사용하기 때문에 ui 패키지에 설정 파일과 실행 스크립트를 작성함

---
+++ packages/ui/src/Button.fixture.tsx
@@ -0,0 +1,30 @@
+import { Button } from "./Button";
+import { color } from "@mozu/design-token";
+
+export default {
+  "Default Button": <Button>기본 버튼</Button>,
+  "Orange Button": (
+    <Button
+      backgroundColor={color.orange[500]}
+      color={color.white}
+      borderColor={color.orange[500]}
+      hoverBackgroundColor={color.orange[600]}
+    >
+      주황색 버튼
+    </Button>
+  ),
+  "Icon Button": (
+    <Button
+      isIcon={true}
+      type="plusImg"
+      iconSize={20}
+      iconColor={color.white}
+      backgroundColor={color.orange[500]}
+      borderColor={color.orange[500]}
+      hoverBackgroundColor={color.orange[600]}
+    >
+      아이콘 버튼
+    </Button>
+  ),
+  "Disabled Button": <Button disabled>비활성화 버튼</Button>,
+};
