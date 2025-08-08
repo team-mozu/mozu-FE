1ca4ca062a88f8bf5f44be47c8bda133d69f34f6
jidohyun <shickcoong777@gmail.com>
2025-08-08 20:12:00 +0900

    fix(ui): vite 빌드 설정 오류 해결

---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,40 @@
+import react from "@vitejs/plugin-react";
+import path, { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "UI",
+      formats: [
+        "es",
+        "cjs",
+      ],
+      fileName: format => `index.${format === "es" ? "js" : "cjs"}`,
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+      ],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(process.cwd(), "./src"),
+      "@mozu/design-token": path.resolve(__dirname, "../design-token/src"),
+    },
+  },
+});
